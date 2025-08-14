b791ac2c3927fead73fa322f741c98953b8d72a0
Mark Hubbard <markhub@gmail.com>
2025-08-14 13:57:28 +1200

    added featires

--- PYSample.py
+++ PYSample.py
@@ -1,5 +1,6 @@
 print (" -----------    ");
 print ("Python has entered the building");
-print ("Let the party begin");
+print ("Let the party begin !!!!!");
 print ("warning: party may be loud");
 print (" Danger ");
+print (" Danger Danger ");
